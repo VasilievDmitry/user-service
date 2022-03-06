1c262b053eed9e3098b6594cfb2bf6a32bd5b0d9
pr0head <pr0head@gmail.com>
2022-03-06 07:06:31 +0300

    Исправить описание конфигурационных параметров и их установку в override.conf

--- config/config.go
+++ config/config.go
@@ -20,7 +20,7 @@ type Config struct {
 	MigrationsLockTimeout int64  `envconfig:"MIGRATIONS_LOCK_TIMEOUT" default:"120"`
 
 	BcryptCost               int    `envconfig:"BCRYPT_COST" required:"false" default:"10"`
-	RefreshTokenLifetime     int    `envconfig:"REFRESH_TOKEN_LIFETIME" required:"false" default:"365"`
+	RefreshTokenLifetime     int    `envconfig:"REFRESH_TOKEN_LIFETIME" required:"false" default:"30"`
 	AccessTokenLifetime      int    `envconfig:"ACCESS_TOKEN_LIFETIME" required:"false" default:"3"`
 	AccessTokenSecret        string `envconfig:"ACCESS_TOKEN_SECRET" required:"true"`
 	AccessTokenSigningMethod string `envconfig:"ACCESS_TOKEN_SIGNING_METHOD" required:"false" default:"HS256"`
