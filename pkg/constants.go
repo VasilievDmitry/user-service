7468898cf6a05db59e08f52977e2065b0dfc2c06
pr0head <pr0head@gmail.com>
2022-03-19 11:18:31 +0300

    Перенести константы и структуры из go-proto в локальную папку

---
+++ pkg/constants.go
@@ -0,0 +1,33 @@
+package pkg
+
+const (
+	ServiceName = "LOT_USER_SERVICE"
+
+	ErrorInternalError           = "internal_server_error"
+	ErrorUserNotFound            = "user_not_found"
+	ErrorWalletNotFound          = "wallet_not_found"
+	ErrorAuthenticationNotFound  = "authentication_not_found"
+	ErrorLoginAlreadyConfirmed   = "login_already_confirmed"
+	ErrorLoginAlreadyExists      = "login_already_exists"
+	ErrorRecoveryCodeInvalid     = "recovery_code_invalid"
+	ErrorConfirmationCodeInvalid = "confirmation_code_invalid"
+	ErrorInvalidPassword         = "invalid_password"
+	ErrorTokenOwnerInvalid       = "token_owner_invalid"
+	ErrorWalletUnsupportedType   = "wallet_unsupported_type"
+
+	WalletTypePhantom = "phantom"
+)
+
+var (
+	WalletList = map[string]bool{
+		WalletTypePhantom: true,
+	}
+)
+
+func IsSupportedWalletType(t string) bool {
+	if _, ok := WalletList[t]; !ok {
+		return false
+	}
+
+	return true
+}
---
+++ pkg/go.mod
@@ -0,0 +1,10 @@
+module github.com/lotproject/user-service/pkg
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/golang/protobuf v1.5.2
+	github.com/micro/go-micro v1.18.0
+	google.golang.org/protobuf v1.27.1
+)
